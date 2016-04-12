5f0b73c009497ad8ef37976e48c82a2fb2cf6154
이동욱 <jojoldu@zuminternet.com>
2016-04-12 18:15:16 +0900

    회원, 커밋 controller 생성

---
+++ src/main/java/devplanet/pojo/CommitResponse.java
@@ -0,0 +1,10 @@
+package devplanet.pojo;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/**
+ * Created by jojoldu@zuminternet.com on 2016-04-11.
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CommitResponse {
+}
