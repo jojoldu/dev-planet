72fa8dfba1dbfbbadfb86a1183f322b962aebd0d
jojoldu <jojoldu@gmail.com>
2016-07-08 08:41:03 +0900

    테스트 대상 추가 (커밋체크 마지막날짜를 기록할필요가 있을까 고민.. 커밋은 되어있어도 contribution에 나중에 반영될수 있어서 항상 100일 체크를 하는게 정확할것 같음)

--- src/test/java/UserServiceTest.java
+++ src/test/java/UserServiceTest.java
@@ -1,10 +1,13 @@
 import devplanet.model.User;
 import devplanet.service.UserService;
 import devplanet.service.impl.UserServiceImpl;
+import org.joda.time.DateTime;
 import org.junit.Before;
 import org.junit.Test;
 
 import javax.annotation.PostConstruct;
+import java.util.Date;
+
 import static org.junit.Assert.*;
 /**
  * Created by jojoldu@gmail.com on 2016-07-08.
@@ -24,5 +27,6 @@ public class UserServiceTest {
         user.setUserName("sungjine456");
         User settedUser = userService.setStreakByGithub(user);
         assertEquals(settedUser.getCurrentStreak(), 46);
+        assertEquals(settedUser.getLastCheckDate().getMillis(), new DateTime("2016-05-23").getMillis());
     }
 }
